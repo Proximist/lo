d0c5af5379ae5eb3b2912e5dbc7935f557550a5f
Proximist <103359694+Proximist@users.noreply.github.com>
2024-10-19 20:32:13 +0530

    Update page.tsx

--- app/task/page.tsx
+++ app/task/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
-import styles from './task.module.css';
+import styles from './task.module.cs';
 
 const Task: React.FC = () => {
   return (
