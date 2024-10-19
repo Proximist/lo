caa53172885ef2af7fb78c5bc1d7221c06147c3c
Proximist <103359694+Proximist@users.noreply.github.com>
2024-10-19 21:33:56 +0530

    Update TaskUI.tsx

--- app/task/TaskUI.tsx
+++ app/task/TaskUI.tsx
@@ -37,7 +37,6 @@ export default function HomeUI({
     link.rel = 'stylesheet';
     link.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css';
     document.head.appendChild(link);
-    toggleUpdateText();
   }, []);
 
   return (
